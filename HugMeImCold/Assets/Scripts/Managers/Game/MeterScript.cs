8ce40deb97441539b43b5662c523931f601fa957
ellie <e.ivanova.10@aberdeen.ac.uk>
2017-12-03 00:19:33 +0000

    Initial work on meters

---
+++ HugMeImCold/Assets/Scripts/Managers/Game/MeterScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeterScript : MonoBehaviour
+{
+    float status;
+    public float decrease = 0.01f;
+    public float coldness = 100f;
+
+    public float Status { get { return status; } }
+
+	// Use this for initialization
+	void Start () {
+        status = 1000f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        status -= decrease + coldness * 0.01f;
+        Debug.Log(status);
+	}
+
+}
